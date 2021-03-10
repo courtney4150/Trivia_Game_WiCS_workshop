34e428458f6ffa7e50fb8c9f94faf6c522797681
courtney4150 <34966195+courtney4150@users.noreply.github.com>
2021-03-09 20:07:31 -0500

    Initial Commit

---
+++ WiCS Trivia 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WiCS Trivia 2
+//
+//  Created by Courtney Sheridan on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WiCS Trivia 2/WiCS_Trivia_2App.swift
@@ -0,0 +1,17 @@
+//
+//  WiCS_Trivia_2App.swift
+//  WiCS Trivia 2
+//
+//  Created by Courtney Sheridan on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WiCS_Trivia_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
